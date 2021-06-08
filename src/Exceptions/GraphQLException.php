a440037850b1927af6a42f06647efb332baf3d7b
xurh <xurhwork@gmail.com>
2021-06-08 12:54:49 +0800

    ✨ feat: 新增GraphQL错误

---
+++ src/Exceptions/GraphQLException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace QT\GraphQL\Exceptions;
+
+use RuntimeException;
+
+/**
+ * GraphQLException
+ * 
+ * @package QT\GraphQL\Exceptions
+ */
+class GraphQLException extends RuntimeException
+{
+
+}
