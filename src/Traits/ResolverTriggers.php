365466058f4c1d11e2819bcaa9b196952719cfc4
xurh <xurhwork@gmail.com>
2022-02-16 15:26:28 +0800

    ✨ feat: 添加beforeQuery,使得查询列表与详情可以共用同一筛选条件

--- src/Traits/ResolverTriggers.php
+++ src/Traits/ResolverTriggers.php
@@ -8,57 +8,143 @@ use QT\GraphQL\Contracts\Context;
 use QT\GraphQL\Options\JsonOption;
 use QT\GraphQL\Options\CursorOption;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 
 trait ResolverTriggers
 {
-    protected function beforeList(Context $context, JsonOption $option)
+    /**
+     * 在查询前触发
+     * 
+     * @param Context $context
+     */
+    protected function beforeQuery(Context $context)
     {
+
     }
 
+    /**
+     * 在查询列表前触发
+     * 
+     * @param Context $context
+     * @param JsonOption $option
+     */
+    protected function beforeList(Context $context, JsonOption $option)
+    {
+        $this->beforeQuery($context);
+    }
+
+    /**
+     * 列表查询后触发
+     * 
+     * @param Collection $models
+     */
     protected function afterList($models)
     {
         return $models;
     }
 
+    /**
+     * 查询单条记录前触发
+     * 
+     * @param Context $context
+     * @param int|string $id
+     */
     protected function beforeShow(Context $context, int|string $id)
     {
+        $this->beforeQuery($context);
     }
 
+    /**
+     * 查询单条记录后触发
+     * 
+     * @param Model $model
+     */
     protected function afterShow($model)
     {
         return $model;
     }
 
-    protected function beforeUpdate(Context $context, Model $model, array $input)
-    {
-    }
-
-    protected function afterUpdate($model)
-    {
-    }
-
+    /**
+     * 创建记录前触发
+     * 
+     * @param Context $context
+     * @param Model $model
+     * @param array $input
+     */
     protected function beforeStore(Context $context, Model $model, array $input)
     {
+
     }
 
+    /**
+     * 创建记录后触发
+     * 
+     * @param Model $model
+     */
     protected function afterStore($model)
     {
+
     }
 
+    /**
+     * 修改记录前触发
+     * 
+     * @param Context $context
+     * @param Model $model
+     * @param array $input
+     */
+    protected function beforeUpdate(Context $context, Model $model, array $input)
+    {
+
+    }
+
+    /**
+     * 修改记录后触发
+     * 
+     * @param Model $model
+     */
+    protected function afterUpdate($model)
+    {
+
+    }
+
+    /**
+     * 删除记录前触发
+     * 
+     * @param Context $context
+     * @param Model $model
+     */
     protected function beforeDestroy(Context $context, Model $model)
     {
+        
     }
 
+    /**
+     * 删除记录前触发
+     * 
+     * @param Model $model
+     */
     protected function afterDestroy($model)
     {
         return $model;
     }
 
+    /**
+     * 批量删除前触发
+     * 
+     * @param Collection $models
+     */
     protected function beforeBatchDestroy($models)
     {
 
     }
 
+    /**
+     * 导出列表前触发
+     * 
+     * @param Context $context
+     * @param CursorOption $option
+     */
     protected function beforeExport(Context $context, CursorOption $option)
     {
         $this->beforeList($context, $option);
